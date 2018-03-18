9bf319e744ec6f58f42b84ae711cd13cfe156af7
Brendon Smith <bsmith@akiliinteractive.com>
2018-03-17 17:39:53 -0700

    locaction change

--- helpers/wpContent.js
+++ helpers/wpContent.js
@@ -4,15 +4,15 @@ const root = Config.ROOT
 const site = Config.SITE
 
 export const placeHolderImg = [
-  `${root}images/placeholder/sc9Img1.jpg`,
-  `${root}images/placeholder/sc9Img2.jpg`,
-  `${root}images/placeholder/sc9Img3.jpg`,
-  `${root}images/placeholder/sc9Img4.jpg`,
-  `${root}images/placeholder/sc9Img5.jpg`,
-  `${root}images/placeholder/sc9Img6.jpg`,
-  `${root}images/placeholder/sc9Img7.jpg`,
-  `${root}images/placeholder/sc9Img8.jpg`,
-  `${root}images/placeholder/sc9Img9.jpg`
+  `${root}Images/placeholder/sc9Img1.jpg`,
+  `${root}Images/placeholder/sc9Img2.jpg`,
+  `${root}Images/placeholder/sc9Img3.jpg`,
+  `${root}Images/placeholder/sc9Img4.jpg`,
+  `${root}Images/placeholder/sc9Img5.jpg`,
+  `${root}Images/placeholder/sc9Img6.jpg`,
+  `${root}Images/placeholder/sc9Img7.jpg`,
+  `${root}Images/placeholder/sc9Img8.jpg`,
+  `${root}Images/placeholder/sc9Img9.jpg`
 ]
 
 export const wpContent = (value) => {
