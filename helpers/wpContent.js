189d243796510bd208354bf9c96cc2b24e14d6e9
Brendon Smith <bsmith@akiliinteractive.com>
2018-06-22 15:39:43 -0700

    wp-react-core
    - only returning an item to render if has a title

--- helpers/wpContent.js
+++ helpers/wpContent.js
@@ -6,12 +6,16 @@ const site = Config.SITE
 export const placeHolderImg = Config.placeHolder
 
 export const wpContent = (value) => {
-  return {
-    title: value.title.rendered,
-    link: value.link.replace(`${site}`, ''),
-    slug: value.slug,
-    excerpt: value.excerpt.rendered,
-    body: value.content.rendered,
-    image: (value.better_featured_image && value.better_featured_image.media_details.sizes['medium_large'] ? value.better_featured_image.media_details.sizes['medium_large'].source_url : randomItem(placeHolderImg))
+  if (value.title.rendered) {
+    return {
+      title: value.title.rendered,
+      link: value.link.replace(`${site}`, ''),
+      slug: value.slug,
+      excerpt: value.excerpt.rendered,
+      body: value.content.rendered,
+      image: (value.better_featured_image && value.better_featured_image.media_details.sizes['medium_large'] ? value.better_featured_image.media_details.sizes['medium_large'].source_url : randomItem(placeHolderImg))
+    }
+  }else{
+    null
   }
 }
